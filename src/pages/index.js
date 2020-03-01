1161853a9452c85aa8b9fba4fb1b6eca2214f951
Gabriel Baker <me@g403.co>
2020-03-01 19:29:57 +0000

    Minor updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>Hi blog viewers</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>This has amazing branch builds thanks to my wonderful developer.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
